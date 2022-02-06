10d07c6b0629b49a544a0f6ccea91d73bc445456
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-02-07 04:21:27 +0900

    test

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+const (
+	commitMsgFilePath = ".git/COMMIT_EDITMSG"
+)
+
+var (
+	FormatRegularPattern = `([a-zA-Z]+)\((.+)\):\s(.*)`
+)
+
+type Format struct {
+	Type    string
+	Scope   string
+	Subject string
+}
+
+func messageParser(m string) {
+
+}
 
 func main() {
 	fmt.Println("pre-commit !!!!  binary message")
+	p, err := regexp.Compile(FormatRegularPattern)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := os.ReadFile(commitMsgFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bbb := p.FindAllSubmatch(b, 1)
+	fmt.Println(bbb)
+
+	os.Exit(1)
 }
